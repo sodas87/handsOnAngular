10310d7074b0213ea9e03bba7f0f1def32dee1bf
Sourav Das <das.souravdas1987@gmail.com>
2025-06-07 12:56:57 -0400

    NG On Change hook interception committed

--- testProject2/src/app/in-out-child-sample/in-out-child-sample.component.ts
+++ testProject2/src/app/in-out-child-sample/in-out-child-sample.component.ts
@@ -1,12 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-in-out-child-sample',
-  imports: [],
-  templateUrl: './in-out-child-sample.component.html',
+  imports: [NgFor],
+  // template: ` <p>Changes Log:</p>
+  //    <ul>
+  //     <li *ngFor="let log of changesLog">{{ log }}</li>
+  //   </ul>`,
+ templateUrl: './in-out-child-sample.component.html',
   styleUrl: './in-out-child-sample.component.css'
 })
-export class InOutChildSampleComponent {
+export class InOutChildSampleComponent implements OnChanges {
   @Input() counter:number=0;
+  changesLog: string[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    
+   for (const key in changes) {
+      const prop = changes[key];
+      
+      const prevVal = prop.previousValue
+      const currentVal = prop.currentValue
+      var isFirstChange = prop.isFirstChange()
+      
+      if (isFirstChange) {
+         console.log("The current value is"+currentVal);
+         this.changesLog.push("The current value is "+currentVal);
+      } else {
+         console.log(`${key} changed from `+ prevVal +" to " +currentVal);
+         this.changesLog.push(`${key} changed from `+ prevVal +" to " +currentVal);
+      }
+   
+   }
+  }
+
 
 }
